796959b51274846727ace6b6e351f4eb5923fa55
Jack D. Douglas <cincomc@proton.me>
2025-06-25 12:12:52 +0100

    fix(ai): TON-1146: increase max memory threshold

--- workers/ai/worker.config.js
+++ workers/ai/worker.config.js
@@ -21,7 +21,7 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: false,
-    max_memory_restart: "500M",
+    max_memory_restart: "5G",
     env: {
       NODE_ENV: "production",
     },
